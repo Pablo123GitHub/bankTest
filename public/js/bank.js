3b93b759fff4745ff6ad9fb5e27914f0eadb403b
Pablo <paul.ladiv123@gmail.com>
2017-11-27 16:44:38 +0000

    test the _transactions array : first element must be timeStamp

--- public/js/bank.js
+++ public/js/bank.js
@@ -5,25 +5,53 @@
 (function(exports) {
   function Bank() {
     this._statement = 0;
+    this._arrWithTransactions = [];
+    this._transactions = []
+
   };
 
   Bank.prototype = {
     deposit: function(amount) {
       this._statement += amount;
+      var timeNow = Date.now();
+      var d = new Date(parseInt(timeNow, 10));
+      var ds = d.toString('MM/dd/yy HH:mm:ss');
+      this._transactions.push(ds);
+      this._arrWithTransactions.push(this._transactions);
 
     },
+
     withdraw: function(amount) {
       this._statement -= amount;
     },
+
     showBalance: function() {
       return this._statement;
     },
 
     returnDate: function(timeInSecond) {
+
       var d = new Date(parseInt(timeInSecond, 10));
       var ds = d.toString('MM/dd/yy HH:mm:ss');
-return ds
-    }
+      return ds
+    },
+
+    showTransactionArr: function() {
+      return this._arrWithTransactions;
+    },
+
+    showTransaction: function() {
+      return this._transactions;
+    },
+
+
+    insertTransactionInArr: function(transArr) {
+      this._arrWithTransactions.push(transArr);
+    },
+
+    insertTransaction: function(transaction) {
+      this.transactions.push(transaction)
+    },
 
 
 
--- spec/frontend/unit/bankSpec.js
+++ spec/frontend/unit/bankSpec.js
@@ -45,6 +45,45 @@ describe("Bank", function() {
 
     });
 
+    it("adds a new transaction to internal array arrTransaction when any transaction is performed", function() {
+
+      bank.deposit(1000);
+      bank.deposit(2000);
+      var transactionTwo = bank.showTransaction();
+      // console.log(transactionTwo);
+      expect(transactionTwo.length).toEqual(2);
+
+
+    });
+
+    it("first element of the _transactions array is date/timeStamp now", function() {
+
+      // var Date = {
+      //   now: function(value) {
+      //     bar = value;
+      //   }
+      // };
+      //
+      // spyOn(Date, "now").and.returnValue(1511791808370);
+
+      bank.deposit(1000);
+      var transaction = bank.showTransaction();
+
+      var timeNow = Date.now();
+      var d = new Date(parseInt(timeNow, 10));
+      var ds = d.toString('MM/dd/yy HH:mm:ss');
+
+      // expect(Date.now()).toEqual(1511791808370);
+      expect(transaction[0]).toEqual(ds);
+
+    });
+
+
+    it("I ll find out ", function() {
+
+
+    });
+
 
   });
 });
